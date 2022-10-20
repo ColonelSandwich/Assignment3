9d96cb4bc30d1a630ca176658f93a1dfa974a4cb
TerrySanders256 <59904544+ColonelSandwich@users.noreply.github.com>
2022-10-20 02:55:35 +0000

    Added JS text to the canvas on page 1

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Initialize upon loading the window
-window.onload = init;
-function init() {
-  getYearCurrent();
-}
-
 // Get the current year and set that to the maximum value for year
 function getYearCurrent() {
   const d = new Date();
@@ -23,5 +17,30 @@ function validateForm() {
   if (pass1 != pass2) {
     alert("Passwords don't match!");
     return false;
+  }
+}
+
+function drawText() {
+
+  // Get the canvas
+  var canvas = document.getElementById("p1canvas");
+
+  // Make sure the canvas actually exists!
+  if (canvas.getContext) {
+
+    // use getContext to use the canvas for drawing
+    var ctx = canvas.getContext("2d");
+
+    // Set font
+    ctx.font = "20px Comic Sans MS";
+
+    // Fill with red
+    ctx.fillStyle = "hotpink";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Center white text
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("Now it is TEXT TIME!", canvas.width / 2, canvas.height / 2);
   }
 }
